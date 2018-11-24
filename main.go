dcb76ffd66182734067003052f0b9b68408732a3
Howard Kitto <kitto.howard@gmail.com>
2018-11-24 08:05:06 +0000

    create new transaction

--- main.go
+++ main.go
@@ -2,23 +2,13 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	// "fmt"
 
 	// "encoding/hex"
-	"math/rand"
 	"net/http"
-	"time"
 
 	log "github.com/Sirupsen/logrus"
 )
-
-//Config - server setup details
-type Config struct {
-	DirectoryServer string `json:"directoryServer"`
-}
-
-//ServerConfig - config details for this sever
-var ServerConfig Config
 
 func logRequest(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -29,81 +19,33 @@ func logRequest(handler http.Handler) http.Handler {
 
 func handleTransaction(w http.ResponseWriter, r *http.Request) {
 
-	switch r.Method {
-	case http.MethodGet:
-		// Serve the resource.
-	case http.MethodPost:
-		transaction := CreateTransaction()
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(transaction)
-	case http.MethodPut:
-		// Update an existing record.
-	case http.MethodDelete:
-		// Remove the record.
-	default:
-		// Give an error message.
-	}
-}
-
-func handleServerConfig(w http.ResponseWriter, r *http.Request) {
+	var tx Transaction
+	var err error
 
 	switch r.Method {
-
+	
 	case http.MethodPost:
 		decoder := json.NewDecoder(r.Body)
-		err := decoder.Decode(&ServerConfig)
-		if err != nil {
+		err = decoder.Decode(&tx)
+		tx, err = CreateTransaction(tx)
+		if err != nil{
 			log.Warn(err)
 		}
-		go pollForTransactions()
+
 	case http.MethodPut:
-		// Update an existing record.
+		
 	case http.MethodDelete:
-		ServerConfig.DirectoryServer = ""
+		
 	default:
-		// Give an error message.
+		
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(ServerConfig)
-
-}
-
-func handleGetKeys(w http.ResponseWriter, r *http.Request) {
-
-	// p := parameters{number_of_participants: 10, threshold: 5}
-	// InitContext(p)
-
-	// pK, sK := Keygen()
-
-	// ServerConfig.TRSPublicKey = hex.EncodeToString(pK)
-	// ServerConfig.TRSPrivateKey = hex.EncodeToString(sK)
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(ServerConfig)
-
-}
-
-func pollForTransactions() {
-
-	directoryString := "http://" + ServerConfig.DirectoryServer + "/transaction"
-
-	for ServerConfig.DirectoryServer != "" {
-		txResp, err := http.Get(directoryString)
-		if err != nil {
-			log.Warn(err)
-		} else {
-			fmt.Printf("txResp %v", &txResp)
-		}
-		pollInterval := rand.Intn(5) * 1000
-		time.Sleep(time.Duration(pollInterval) * time.Millisecond)
-	}
+	json.NewEncoder(w).Encode(tx)
 }
 
 func main() {
 
-	http.HandleFunc("/serverconfig", handleServerConfig)
-	http.HandleFunc("/keys", handleGetKeys)
 	http.HandleFunc("/transaction", handleTransaction)
 
 	if err := http.ListenAndServe(":5000", logRequest(http.DefaultServeMux)); err != nil {
@@ -111,3 +53,91 @@ func main() {
 	}
 
 }
+
+// //ServerConfig - config details for this sever
+// var ServerConfig Config
+
+// func handleTransaction(w http.ResponseWriter, r *http.Request) {
+
+// 	switch r.Method {
+// 	case http.MethodGet:
+// 		// Serve the resource.
+// 	case http.MethodPost:
+// 		transaction := CreateTransaction()
+// 		w.Header().Set("Content-Type", "application/json")
+// 		json.NewEncoder(w).Encode(transaction)
+// 	case http.MethodPut:
+// 		// Update an existing record.
+// 	case http.MethodDelete:
+// 		// Remove the record.
+// 	default:
+// 		// Give an error message.
+// 	}
+// }
+
+// func handleServerConfig(w http.ResponseWriter, r *http.Request) {
+
+// 	switch r.Method {
+
+// 	case http.MethodPost:
+// 		decoder := json.NewDecoder(r.Body)
+// 		err := decoder.Decode(&ServerConfig)
+// 		if err != nil {
+// 			log.Warn(err)
+// 		}
+// 		go pollForTransactions()
+// 	case http.MethodPut:
+// 		// Update an existing record.
+// 	case http.MethodDelete:
+// 		ServerConfig.DirectoryServer = ""
+// 	default:
+// 		// Give an error message.
+// 	}
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(ServerConfig)
+
+// }
+
+// func handleGetKeys(w http.ResponseWriter, r *http.Request) {
+
+// 	// p := parameters{number_of_participants: 10, threshold: 5}
+// 	// InitContext(p)
+
+// 	// pK, sK := Keygen()
+
+// 	// ServerConfig.TRSPublicKey = hex.EncodeToString(pK)
+// 	// ServerConfig.TRSPrivateKey = hex.EncodeToString(sK)
+
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(ServerConfig)
+
+// }
+
+// func pollForTransactions() {
+
+// 	directoryString := "http://" + ServerConfig.DirectoryServer + "/transaction"
+
+// 	for ServerConfig.DirectoryServer != "" {
+// 		txResp, err := http.Get(directoryString)
+// 		if err != nil {
+// 			log.Warn(err)
+// 		} else {
+// 			fmt.Printf("txResp %v", &txResp)
+// 		}
+// 		pollInterval := rand.Intn(5) * 1000
+// 		time.Sleep(time.Duration(pollInterval) * time.Millisecond)
+// 	}
+// }
+
+// func main() {
+
+// 	http.HandleFunc("/serverconfig", handleServerConfig)
+// 	http.HandleFunc("/keys", handleGetKeys)
+// 	http.HandleFunc("/transaction", handleTransaction)
+
+// 	if err := http.ListenAndServe(":5000", logRequest(http.DefaultServeMux)); err != nil {
+// 		panic(err)
+// 	}
+
+// }
--- transaction.go
+++ transaction.go
@@ -1,62 +1,85 @@
 package main
 
 import (
-
-	"github.com/google/uuid"
+	"errors"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/google/uuid"
 
 	log "github.com/Sirupsen/logrus"
-
 )
 
 //Transaction - Sruct to compose transactions
 type Transaction struct {
-	UID string `json:"uID"`
-	TXID string`json:"txID"`
+	TxID      string    `json:"txID"`
+	UserID    string    `json:"userID"`
+	SK        string    `json:"sK"`
+	PK        string    `json:"pK"`
+	Signers   []int64   `json:"signers"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	Message   Message   `json:"message"`
+	Policy    Policy    `json:"policy"`
+}
+
+type Message struct {
+	Amount             int64  `json:"amount"`
+	Currency           string `json:"currency"`
+	DestinationAddress string `json:"destinationAddress"`
+}
+
+type Policy struct {
+	Participants []string `json:"participants"`
+	Threshold    int64    `json:"threshold"`
 }
 
 //CreateTransaction - Insert Transaction into Dynamo DB
-func CreateTransaction() (tx Transaction){
+func CreateTransaction(newTX Transaction) (tx Transaction, err error) {
 
-	uID, err := uuid.NewRandom()
-	if err != nil {
-		log.Warn(err)
+	tx = newTX
+
+	if newTX.TxID != "" {
+		errStr := "Found TXID - Not a new transaction"
+		return tx, errors.New(errStr)
 	}
 
 	txID, err := uuid.NewRandom()
 	if err != nil {
-		log.Warn(err)
+		return tx, err
 	}
 
-	tx = Transaction{uID.String(), txID.String()}
-
+	tx.TxID = txID.String()
+	tx.CreatedAt = time.Now()
+	tx.UpdatedAt = time.Now()
 
 	sess, err := session.NewSession(&aws.Config{
-        Region: aws.String("eu-west-2")},
-    )
-
-    // Create DynamoDB client
-    svc := dynamodb.New(sess)
+		Region: aws.String("eu-west-2")},
+	)
+	svc := dynamodb.New(sess)
 
 	info, err := dynamodbattribute.MarshalMap(tx)
-		if err != nil {
-			log.Fatal("failed to marshal the tx", err)
-		}
+	if err != nil {
+		return tx, err
+	}
 
 	input := &dynamodb.PutItemInput{
-			Item:      info,
-			TableName: aws.String("Transactions"),
-		}
+		Item:      info,
+		TableName: aws.String("Transactions"),
+	}
 
 	_, err = svc.PutItem(input)
-		if err != nil {
-			log.Warn(err.Error())
-			return
-		}
-		log.Info("Created Item for ", tx.UID)
-		return tx
+	if err != nil {
+		return tx, err
+	}
+
+	log.Info("Created ", tx.TxID)
+	return tx, err
+}
+
+func ReadTransaction() {
+
 }
