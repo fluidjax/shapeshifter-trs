f85dc59aea35e2dbe68311c58acdfafe75adbf3f
Howard Kitto <kitto.howard@gmail.com>
2018-11-26 08:28:02 +0000

    added server port flag

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"math/rand"
 	"net/http"
+	"strconv"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -89,7 +91,7 @@ func inviteApprovers(tx Transaction) {
 	approvalRequest.Message = tx.Message
 
 	for i, p := range tx.Policy.Participants {
-		// log.Info("Inviting ", tx.Policy.Participants[i].URL)
+		log.Info("Inviting ", tx.Policy.Participants[i].URL)
 		approvalRequest.URL = tx.Policy.Participants[i].URL
 		approvalRequest.RingIndex = i
 
@@ -169,13 +171,13 @@ func approveMessage(approval bool, signingRequest SignerRequest) {
 		client := &http.Client{}
 		request, err := http.NewRequest("PUT", url, bytes.NewBuffer(signingRequestJSON))
 		request.Header.Add("Content-Type", "application/json")
-		resp, err := client.Do(request)
+		_, err = client.Do(request)
 		if err != nil {
 			log.Warn(err)
 		}
 
 		//TODO: this is going to cause a crash on 404
-		defer resp.Body.Close()
+		// defer resp.Body.Close()
 	}
 }
 
@@ -206,11 +208,18 @@ func handleSign(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
+	serverPort := flag.Int("port", 5000, "Server Port")
+
+	flag.Parse()
+
+	port := ":" + strconv.Itoa(*serverPort)
+	log.Info("Server Port = ", port)
+
 	http.HandleFunc("/transaction/", handleTransaction)
 	http.HandleFunc("/approvalrequest", handleApprovalRequest)
 	http.HandleFunc("/participantsign", handleSign)
 
-	if err := http.ListenAndServe(":5000", logRequest(http.DefaultServeMux)); err != nil {
+	if err := http.ListenAndServe(port, logRequest(http.DefaultServeMux)); err != nil {
 		panic(err)
 	}
 }
